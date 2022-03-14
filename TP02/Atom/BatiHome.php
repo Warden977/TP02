5837cbe621ba9cebbf724392e122645ce56c5013
NolanNabeth <90611039+Warden977@users.noreply.github.com>
2022-03-14 12:24:47 +0100

    Add files via upload

---
+++ TP02/Atom/BatiHome.php
@@ -0,0 +1,14 @@
+<?php
+
+Class BatiHome extends maison{
+    
+    function get_Garage() {
+        return $this->garage;
+      }
+
+      function get_chemine() {
+        return $this->chemine;
+      }
+
+
+?>
